36673733f16b35d65e4e14a35e72c658c0143941
Sourav Vijay <souravvijay637@gmail.com>
2026-02-24 09:31:49 +0530

    Minor bug fix

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,10 +1,18 @@
 import json
+import os
 from models import Laptop
 
 
 def load_laptops():
 
-    with open("data/laptops.json", "r") as file:
+    # Get project root directory
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    # Build correct path to data file
+    data_path = os.path.join(base_dir, "data", "laptops.json")
+
+    # Load JSON
+    with open(data_path, "r", encoding="utf-8") as file:
         data = json.load(file)
 
     laptops = []
