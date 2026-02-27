9bdc831ae9ab6c32d319c75c1a14bfbbb02c5601
Sourav Vijay <souravvijay637@gmail.com>
2026-02-27 14:13:48 +0530

    Reinitialize repository correctly and fix project structure

--- src/main.py
+++ src/main.py
@@ -20,25 +20,95 @@ def get_profile():
 
 def get_weights():
 
-    print("\n--- Set Priorities (1=Low, 5=High) ---")
+    print("\n--- Select Your Main Priority ---")
+    print("1. Get Job Quickly")
+    print("2. High Salary Focus")
+    print("3. Easy Learning")
+    print("4. Strong Foundation")
+    print("5. Balanced")
 
-    return {
-        "demand": int(input("Job Demand: ")),
-        "roi": int(input("Career ROI: ")),
-        "time": int(input("Fast Completion: ")),
-        "difficulty": int(input("Difficulty Match: ")),
-        "resources": int(input("Learning Resources: "))
-    }
+    choice = int(input("Enter choice (1-5): "))
+
+    if choice == 1:      # Fast job
+        return {
+            "demand": 5,
+            "roi": 4,
+            "time": 5,
+            "difficulty": 3,
+            "resources": 3
+        }
+
+    elif choice == 2:    # Salary
+        return {
+            "demand": 4,
+            "roi": 5,
+            "time": 3,
+            "difficulty": 4,
+            "resources": 3
+        }
+
+    elif choice == 3:    # Easy
+        return {
+            "demand": 3,
+            "roi": 3,
+            "time": 4,
+            "difficulty": 5,
+            "resources": 4
+        }
+
+    elif choice == 4:    # Foundation
+        return {
+            "demand": 4,
+            "roi": 4,
+            "time": 2,
+            "difficulty": 5,
+            "resources": 5
+        }
+
+    else:               # Balanced
+        return {
+            "demand": 4,
+            "roi": 4,
+            "time": 4,
+            "difficulty": 4,
+            "resources": 4
+        }
 
 
 def main():
 
-    print("==== Learning Path Decision Companion ====")
+    print("==== Learning Path Decision Companion ====\n")
 
-    profile = get_profile()
-    weights = get_weights()
+    print("Select Mode:")
+    print("1. Get Recommendation")
+    print("2. Choose Path Manually")
+
+    mode = int(input("Enter choice (1-2): "))
 
     paths = load_paths()
+
+    # Manual Mode
+    if mode == 2:
+
+        print("\nAvailable Paths:\n")
+
+        for i, p in enumerate(paths, 1):
+            print(f"{i}. {p['name']}")
+
+        choice = int(input("\nSelect a path: "))
+
+        selected = paths[choice - 1]
+
+        print("\nYou Selected:")
+        print(selected["name"])
+        print(selected["description"])
+
+        return
+
+
+    # Recommendation Mode
+    profile = get_profile()
+    weights = get_weights()
 
     ranked = rank_paths(paths, weights, profile)
 
